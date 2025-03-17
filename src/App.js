31d69dfa264cb55c576deff4d36068e61f5fd810
zzzmini <sm01079200887@gmail.com>
2025-03-17 16:07:27 +0900

    front first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+function App() {
+  const [currentTime, setCurrentTime] = useState('');
+  useEffect(() => {
+    // /api/time 엔드포인트로 GET 요청을 보냄
+    axios.get('http://localhost:8080/api/time')
+      .then(response => {
+        setCurrentTime(response.data);
+      })
+      .catch(error => {
+        console.error('API 호출 중 오류 발생:', error);
+      });
+  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 실행
+  
+  return (
+    <div className="App">
+      <div className="App"><h1>현재 시간</h1><p>{currentTime}</p></div>
+    </div>
+  );
+}
+
+export default App;
